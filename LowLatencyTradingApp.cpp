dfb727cf5dab966ef985a56355606f580615d8d9
tranvantuan <trantuan22052k@gmail.com>
2024-10-06 03:48:06 +0800

    Add .gitattributes, .gitignore, README.md, and LICENSE.txt.

---
+++ LowLatencyTradingApp.cpp
@@ -0,0 +1,12 @@
+﻿// LowLatencyTradingApp.cpp : Defines the entry point for the application.
+//
+
+#include "LowLatencyTradingApp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
