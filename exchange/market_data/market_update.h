a3277883031cbbeb2e910cae307a7ab35bd98f63
tranvantuan <trantuan22052k@gmail.com>
2024-10-11 02:54:24 +0800

    update market_date

--- exchange/market_data/market_update.h
+++ exchange/market_data/market_update.h
@@ -1,61 +1,72 @@
-#pragma once 
+// - `inline` giúp giảm chi phí gọi hàm bằng các sao chép
+// trục tiếp mã của hàm tại vị trí gọi vì thực hiện lời gọi
+// hàm. Điều này hữu dụng khi hàm nhỏ, và lời gọi hàm nhiều
+// lần có thể làm chậm chương trình 
+// - Cải thiện khả năng tối ưu hóa: Trình biên dịch có thể 
+// tận dụng các tối ưu hóa khách khi hàm được mở rộng trực 
+// tiếp tại nơi nó được gọi. 
+
+#pragma once
+
 #include <sstream>
+
 #include "common/types.h"
-#include "common/lf_queue.h"
 
 using namespace Common;
 
-namespace Exchage {
-    #pragma pack(push, 1)
-        enum class MarketUpdateType : uint8_t {
-            INVALID = 0, 
-            ADD = 1, 
-            MODIFY = 2, 
-            CANCEL = 3, 
-            TRADE = 4, 
-        };
-        // - `inline` giúp giảm chi phí gọi hàm bằng các sao chép
-        // trục tiếp mã của hàm tại vị trí gọi vì thực hiện lời gọi
-        // hàm. Điều này hữu dụng khi hàm nhỏ, và lời gọi hàm nhiều
-        // lần có thể làm chậm chương trình 
-        // - Cải thiện khả năng tối ưu hóa: Trình biên dịch có thể 
-        // tận dụng các tối ưu hóa khách khi hàm được mở rộng trực 
-        // tiếp tại nơi nó được gọi. 
-        inline std::string marketUpdateTypeToString(MarketUpdateType type) {
-            switch (type) {
-                case MarketUpdateType::ADD: return "ADD";
-                case MarketUpdateType::MODIFY: return "MODIFY";
-                case MarketUpdateType::CANCEL: return "CANCEL";
-                case MarketUpdateType::TRADE: return "TRADE";
-                case MarketUpdateType::INVALID: return "INVALID";
-            }
-            return "UNKNOWN"; 
+namespace Exchange {
+#pragma pack(push, 1)
+    enum class MarketUpdateType : uint8_t {
+        INVALID = 0,
+        ADD = 1,
+        MODIFY = 2,
+        CANCEL = 3,
+        TRADE = 4
+    };
+
+    inline std::string marketUpdateTypeToString(MarketUpdateType type) {
+        switch (type) {
+        case MarketUpdateType::ADD:
+            return "ADD";
+        case MarketUpdateType::MODIFY:
+            return "MODIFY";
+        case MarketUpdateType::CANCEL:
+            return "CANCEL";
+        case MarketUpdateType::TRADE:
+            return "TRADE";
+        case MarketUpdateType::INVALID:
+            return "INVALID";
         }
-        struct MEMarketUpdate {
-            MarketUpdateType type_ = MarketUpdateType::INVALID;
+        return "UNKNOWN";
+    }
 
-            OrderId order_id_ = OrderId_INVALID;
-            TickerId ticker_id_ = TickerId_INVALID;
-            Side side_ = Side::INVALID;
-            Price price_ = Price_INVALID;
-            Qty qty_ = Qty_INVALID;
-            Priority priority_ = Priority_INVALID;
+    struct MEMarketUpdate {
+        MarketUpdateType type_ = MarketUpdateType::INVALID;
 
-            auto toString() const {
-                std::stringstream ss;
-                ss << "MEMarketUpdate"
-                    << " ["
-                    << " type:" << marketUpdateTypeToString(type_)
-                    << " ticker:" << tickerIdToString(ticker_id_)
-                    << " oid:" << orderIdToString(order_id_)
-                    << " side:" << sideToString(side_)
-                    << " qty:" << qtyToString(qty_)
-                    << " price:" << priceToString(price_)
-                    << " priority:" << priorityToString(priority_)
-                    << "]";
-                return ss.str();
-            }
-        };
+        OrderId order_id_ = OrderId_INVALID;
+        TickerId ticker_id_ = TickerId_INVALID;
+        Side side_ = Side::INVALID;
+        Price price_ = Price_INVALID;
+        Qty qty_ = Qty_INVALID;
+        Priority priority_ = Priority_INVALID;
+
+        auto toString() const {
+        std::stringstream ss;
+        ss << "MEMarketUpdate"
+            << " ["
+            << " type:" << marketUpdateTypeToString(type_)
+            << " ticker:" << tickerIdToString(ticker_id_)
+            << " oid:" << orderIdToString(order_id_)
+            << " side:" << sideToString(side_)
+            << " qty:" << qtyToString(qty_)
+            << " price:" << priceToString(price_)
+            << " priority:" << priorityToString(priority_)
+            << "]";
+        return ss.str();
+        }
+    };
+
     #pragma pack(pop)
-        typedef LFQueue<MEMarketUpdate> MEMarketUpdateLFQueue;
+
+    typedef Common::LFQueue<Exchange::MEMarketUpdate> MEMarketUpdateLFQueue;
 }
