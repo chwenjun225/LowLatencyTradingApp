f6c74e9937f820a7672b6c94d61c079427979e98
chwenjun225 <trantuan22052k@gmail.com>
2024-11-01 13:02:49 +0800

    Implement OrderServer and FIFOSequencer classes; enhance request handling and logging for improved client request processing

---
+++ exchange/order_server/order_server.cpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <functional>
+
+#include "common/thread_utils.h"
+#include "common/macros.h"
+#include "common/tcp_server.h"
+
+#include "order_server/client_request.h"
+#include "order_server/client_response.h"
+#include "order_server/fifo_sequencer.h"
+
+namespace Exchange {
+    class OrderServer {
+        private:
+            const std::string iface_;
+            const int port_ = 0;
+            ClientResponseLFQueue *outgoing_responses_ = nullptr;
+            volatile bool run_ = false;
+            std::string time_str_;
+            Logger logger_;
+            std::array<size_t, ME_MAX_NUM_CLIENTS> cid_next_outgoing_seq_num_;
+            std::array<size_t, ME_MAX_NUM_CLIENTS> cid_next_exp_seq_num_;
+            std::array<Common::TCPSocket *, ME_MAX_NUM_CLIENTS> cid_tcp_socket_;
+            Common::TCPServer tcp_server_;
+            FIFOSequencer fifo_sequencer_;
+    };
+}
